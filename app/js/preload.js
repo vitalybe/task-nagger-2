ede702ad838ac38f4d637dc688a842ceabce5502
vitaly - git <vitaly@secthor.com>
2016-02-01 16:07:22 +0200

    reading task count from RTM

--- app/js/preload.js
+++ app/js/preload.js
@@ -1,10 +1,23 @@
 (function () {
     var ipcRenderer = require('electron').ipcRenderer;
 
+    function updatePendingTasks() {
+
+        var listItem = document.querySelector('[href="#list/40296046"]');
+        if (listItem) {
+            var taskCountString = listItem.querySelector(".zl-Uk-yo").innerText;
+            var taskCount = taskCountString != "" ? parseInt(taskCountString) : 0;
+            ipcRenderer.send('updatePendingTasks', {count: parseInt(taskCount)});
+        } else {
+            console.warn("List item not found")
+        }
+    }
+
     console.log("Waiting for load");
     document.addEventListener('DOMContentLoaded', function () {
         console.log("Loaded!");
-        ipcRenderer.send('updatePendingTasks', {count: 4});
 
+        updatePendingTasks();
+        setInterval(updatePendingTasks, 5000);
     }, false);
 })();
