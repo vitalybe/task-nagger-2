20cdcbad6fe9e425846fb95af42055a96f4972aa
vitaly - git <vitaly@secthor.com>
2016-02-02 10:36:33 +0200

    clearing the menu

--- app/menu.js
+++ app/menu.js
@@ -1,76 +1,18 @@
-(function(scope) {
+(function (scope) {
     "use strict";
 
     module.exports = [
         {
-            label: 'Task nagger 2',
+            label: 'Task nagger',
             submenu: [
                 {
-                    label: 'About Task nagger 2',
-                    selector: 'orderFrontStandardAboutPanel:'
+                    label: 'Settings',
+                    accelerator: 'CmdOrCtrl+,',
+                    click: function () {
+                        global.settings.init();
+                    }
                 },
-                { type: 'separator' },
-                {
-                    label: 'Hide Task nagger 2',
-                    accelerator: 'CmdOrCtrl+H',
-                    selector: 'hide:'
-                },
-                {
-                    label: 'Hide Others',
-                    accelerator: 'CmdOrCtrl+Shift+H',
-                    selector: 'hideOtherApplications:'
-                },
-                {
-                    label: 'Show All',
-                    selector: 'unhideAllApplications:'
-                },
-                { type: 'separator' },
-                {
-                    label: 'Quit',
-                    accelerator: 'CmdOrCtrl+Q',
-                    selector: 'terminate:'
-                }
-            ]
-        },
-        {
-            label: 'Edit',
-            submenu: [
-                {
-                    label: 'Undo',
-                    accelerator: 'CmdOrCtrl+Z',
-                    selector: 'undo:'
-                },
-                {
-                    label: 'Redo',
-                    accelerator: 'Shift+CmdOrCtrl+Z',
-                    selector: 'redo:'
-                },
-                { type: 'separator' },
-                {
-                    label: 'Cut',
-                    accelerator: 'CmdOrCtrl+X',
-                    selector: 'cut:'
-                },
-                {
-                    label: 'Copy',
-                    accelerator: 'CmdOrCtrl+C',
-                    selector: 'copy:'
-                },
-                {
-                    label: 'Paste',
-                    accelerator: 'CmdOrCtrl+V',
-                    selector: 'paste:'
-                },
-                {
-                    label: 'Select All',
-                    accelerator: 'CmdOrCtrl+A',
-                    selector: 'selectAll:'
-                }
-            ]
-        },
-        {
-            label: 'View',
-            submenu: [
+                {type: 'separator'},
                 {
                     label: 'Reload',
                     accelerator: 'CmdOrCtrl+R',
@@ -82,33 +24,5 @@
                     click: function() { global.taskNagger.window.toggleDevTools(); }
                 }
             ]
-        },
-        {
-            label: 'Window',
-            submenu: [
-                {
-                    label: 'Minimize',
-                    accelerator: 'CmdOrCtrl+M',
-                    selector: 'performMiniaturize:'
-                },
-                {
-                    label: 'Close',
-                    accelerator: 'CmdOrCtrl+W',
-                    selector: 'hide:'
-                },
-                { type: 'separator' },
-                {
-                    label: 'Bring All to Front',
-                    selector: 'arrangeInFront:'
-                },
-                {
-                  label: 'Settings',
-                  accelerator: 'CmdOrCtrl+,',
-                  click: function () {
-                    global.settings.init();
-                  }
-                }
-            ]
-        }
-    ];
+        }];
 })(this);
